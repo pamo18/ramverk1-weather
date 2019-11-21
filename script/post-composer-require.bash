cc934143abd7fa78409f1b934370938d4cfbcd86
Paul Moreland <office@Pauls-iPro-2.home>
2019-11-21 21:37:26 +0100

    Added test config files

--- script/post-composer-require.bash
+++ script/post-composer-require.bash
@@ -2,9 +2,9 @@
 #
 # Install Weather service into an Anax installation using a default setup.
 #
-rsync -av vendor/pamo/ramverk1-weather/config/ config/
-rsync -av vendor/pamo/ramverk1-weather/content/ content/
-rsync -av vendor/pamo/ramverk1-weather/htdocs/ htdocs/
-rsync -av vendor/pamo/ramverk1-weather/src/ src/
-rsync -av vendor/pamo/ramverk1-weather/test/ test/
-rsync -av vendor/pamo/ramverk1-weather/view/ view/
+rsync -av vendor/pamo18/ramverk1-weather/config/ config/
+rsync -av vendor/pamo18/ramverk1-weather/content/ content/
+rsync -av vendor/pamo18/ramverk1-weather/htdocs/ htdocs/
+rsync -av vendor/pamo18/ramverk1-weather/src/ src/
+rsync -av vendor/pamo18/ramverk1-weather/test/ test/
+rsync -av vendor/pamo18/ramverk1-weather/view/ view/
